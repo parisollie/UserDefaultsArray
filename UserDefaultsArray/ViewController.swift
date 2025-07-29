add2057ead2fed1250e523ea65c832e846c51601
parisollie <pjff18@gmail.com>
2025-07-29 10:19:01 -0600

    Pasos mejorados

--- UserDefaultsArray/ViewController.swift
+++ UserDefaultsArray/ViewController.swift
@@ -9,13 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    //V-102,paso 1.0
-
+    //V-102,paso 1.0, hacemos las conexiones
     @IBOutlet weak var text1: UITextField!
     @IBOutlet weak var text2: UITextField!
     @IBOutlet weak var text3: UITextField!
     
-    //Paso 1.1Variable para el uso defaults
+    //Paso 1.1 Variable para el uso defaults
     var array = [String]()
     
     override func viewDidLoad() {
@@ -29,29 +28,30 @@ class ViewController: UIViewController {
         }
     }
 
-    //Paso 1.3 ,Paso para nuestro botton guardar
+    //Paso 1.3 ,Paso para nuestro botón guardar
     @IBAction func guardar(_ sender: UIButton) {
-        //Paso 1.4 .Aqui validamos solo el text 1,aunque lo deberíamos hacer para todos.
+        //Paso 1.4 .Aquí validamos solo el text 1,aunque lo deberíamos hacer para todos.
         if text1.text == "" {
-            //Si esta vacio ,asi validamos nuestro textfield
+            //Si esta vacío ,asi validamos nuestro textfield y le mandamos la alerta.
             alerta(titulo: "Error", mensaje: "Completa todos los campos")
         }else{
-            //(??),valor por defecto pondremos algo vacío
+            //(??),valor por defecto pondremos algo vacío,por si el usuario no puso nada.
             array.append(text1.text ?? "")
             array.append(text2.text ?? "")
             array.append(text3.text ?? "")
-            //Paso 1.5, lo metemos en Userdaults para guardarlo
+            //Paso 1.5, lo metemos en Userdaults para guardarlo.
             UserDefaults.standard.set(array, forKey: "array")
-            //Vemos que se haya guardado
+            //Vemos que se haya guardado.
             print("guardo")
         }
     }
-    //Paso 1.2,Creamos nuestra alerta
+    
+    //Paso 1.2,Creamos nuestra alerta , para poder guardar con información.
     func alerta(titulo: String, mensaje: String){
         
             let alert = UIAlertController(title: titulo, message: mensaje, preferredStyle: .alert)
             
-            //handler: nil,no queremos que haga nada cuando le demos en ok
+            //handler: nil,no queremos que haga nada cuando le demos en OK.
             let ok = UIAlertAction(title: "Okr", style: .default, handler: nil)
             alert.addAction(ok)
             present(alert, animated: true, completion: nil)
